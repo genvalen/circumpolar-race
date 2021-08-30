ba8e76815d38869162805b4f8e4b9769cdce96cf
genvalen <genvalen@protonmail.com>
2021-08-30 02:32:43 -0400

    Update tests

--- script_test.py
+++ script_test.py
@@ -26,19 +26,19 @@ class TestScript(unittest.TestCase):
 
     def test_that_participant_information_is_properly_formatted(self):
         expected = [
-            'Karras, Connie', 
+            "Connie Karras", 
             442.71, 
             418.51, 
             513.05, 
             611.71, 
             470.31, 
             408.6, 
-            436.63, 
-            186.51, 
+            436.63,
             272.41, 
+            186.51, 
             248.56, 
             290.56, 
-            10.75
+            14.06
         ]
 
         data = script.sort_files_by_region("Connie Karras")
